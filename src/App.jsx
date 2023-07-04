ca46eaaad1bd831bffc489728d1006fb1dc0c4bb
Kenneth Velazquez <kenny.arcelay@gmail.com>
2023-07-04 16:48:05 -0500

    changed background color

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,23 @@ function App() {
         </a>
       </div>
       <h1>Kenneth Velázquez</h1>
-      <h2>Frontend Developer</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Frontend Web Developer</h2>
     </>
   )
 }
 
 export default App
+
+
+
+{/* <div className="card">
+  <button onClick={() => setCount((count) => count + 1)}>
+    count is {count}
+  </button>
+  <p>
+    Edit <code>src/App.jsx</code> and save to test HMR
+  </p>
+</div>
+<p className="read-the-docs">
+  Click on the Vite and React logos to learn more
+</p> */}
